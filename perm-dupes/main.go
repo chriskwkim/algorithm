4898e027902a56408699fae91d6889f913024864
Chris Kim <chris.kim@kareo.com>
2020-01-29 20:11:37 -0800

    Added comment

--- perm-dupes/main.go
+++ perm-dupes/main.go
@@ -1,5 +1,10 @@
 package main
 
+/*
+Compute all permutations of a string whose characters are not necessarily unique.
+The list of permutations should not have duplicates.
+*/
+
 import (
 	"fmt"
 )
